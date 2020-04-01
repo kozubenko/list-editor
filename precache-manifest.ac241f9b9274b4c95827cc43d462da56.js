fb8e5a33fae181131391309ab79e342862038107
andriikozubenko <andrii.kozubenko@gmail.com>
2020-04-01 18:24:09 +0300

    Updates

---
+++ precache-manifest.ac241f9b9274b4c95827cc43d462da56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1843e827750b922abd02cf4ad2f0077",
+    "url": "/list-editor/index.html"
+  },
+  {
+    "revision": "7c6f0c810bb50088f9ca",
+    "url": "/list-editor/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5c46d8985d43e248aa1a",
+    "url": "/list-editor/static/js/2.a23b234a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/list-editor/static/js/2.a23b234a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c6f0c810bb50088f9ca",
+    "url": "/list-editor/static/js/main.47e583e8.chunk.js"
+  },
+  {
+    "revision": "22c4e28d36e57e1059cd",
+    "url": "/list-editor/static/js/runtime-main.1d9c571a.js"
+  }
+]);
